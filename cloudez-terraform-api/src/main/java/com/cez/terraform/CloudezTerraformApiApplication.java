f40d223b982c36cfc7af99d42494d5117948af86
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2022-09-15 09:53:46 -0700

    initial commit

---
+++ cloudez-terraform-api/src/main/java/com/cez/terraform/CloudezTerraformApiApplication.java
@@ -0,0 +1,13 @@
+package com.cez.terraform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudezTerraformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudezTerraformApiApplication.class, args);
+	}
+
+}
---
+++ cloudez-terraform-api/src/test/java/com/cez/terraform/CloudezTerraformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cez.terraform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudezTerraformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
