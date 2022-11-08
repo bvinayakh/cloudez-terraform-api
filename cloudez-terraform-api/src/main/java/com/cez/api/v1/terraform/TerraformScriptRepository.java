b9f964c92f8506f78fb78d1e6f7797dce6a1cb9a
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2022-11-07 19:25:28 -0800

    fix dependency error

--- cloudez-terraform-api/src/main/java/com/cez/api/v1/terraform/TerraformScriptRepository.java
+++ cloudez-terraform-api/src/main/java/com/cez/api/v1/terraform/TerraformScriptRepository.java
@@ -1,10 +1,8 @@
 package com.cez.api.v1.terraform;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @org.springframework.stereotype.Repository
 public interface TerraformScriptRepository extends JpaRepository<TerraformScript, String>
 {
-  List searchby(String text, int limit, String... fields);
 }
